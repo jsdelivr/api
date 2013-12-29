16bbbdfc2c0b58a8a88ae5db72d59a1b096b3d2d
Juho Vepsalainen <bebraw@gmail.com>
2013-12-29 21:37:17 +0200

    Add `libraries/<name>/<version>` -> files

--- api.js
+++ api.js
@@ -7,6 +7,28 @@ var Library = require('./schemas').Library;
 module.exports = function(app) {
     var root = '/api/v1';
 
+    app.get(root + '/libraries/:name/:version', function(req, res) {
+        var version = req.params.version;
+
+        sugar.getOne(Library, {
+            name: req.params.name
+        }, function(err, library) {
+            if(err) {
+                return res.send(404);
+            }
+
+            var d = library.assets.filter(function(asset) {
+                return asset.version === version;
+            })[0];
+
+            if(d && d.files) {
+                return res.json(d.files);
+            }
+
+            return res.send(404);
+        });
+    });
+
     app.get(root + '/libraries/:name', function(req, res) {
         sugar.getOne(Library, {
             name: req.params.name
