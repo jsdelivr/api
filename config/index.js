8140e8f37037e4635dec05244ab2f092a12169e3
Juho Vepsalainen <bebraw@gmail.com>
2014-02-08 17:01:38 +0100

    Connect to MongoDB on AppFog

--- config/index.js
+++ config/index.js
@@ -8,4 +8,15 @@ try {
 }
 catch(e) {}
 
-module.exports = parseEnv(process.env, configTemplate, config);
+var env = parseEnv(process.env, configTemplate, config);
+
+if(process.env.VCAP_SERVICES){
+    var vcap = JSON.parse(process.env.VCAP_SERVICES);
+
+    console.log(vcap);
+
+    env.mongo = vcap['mongodb2-2.4.8'][0]['credentials'];
+}
+
+module.exports = env;
+
