20a6e20906687ab8fd0ca350d9ab18e57e472dc7
austin brown <austinjbraun@gmail.com>
2015-04-22 19:49:14 -0700

    Add reference to db name in config

--- config/config.template.js
+++ config/config.template.js
@@ -1,16 +1,18 @@
 module.exports = {
-    port: 8000,
-    //cdns: ['bootstrap', 'cdnjs', 'google', 'jsdelivr', 'jquery'],
-    cdns: ['bootstrap', 'cdnjs', 'google', 'jsdelivr'],
-    syncUrl: 'http://jsdelivrapi-sync.aws.af.cm/data/',
-    tasks: {
-        sync: {minute: 2}
-    },
-    maxcdn: {
-        alias: 'replace this',
-        key: 'replace this',
-        secret: 'replace this',
-        zoneId: 0 // replace with some zone id
-    },
-    github: '' // replace with github token
+  port: 8000,
+  //cdns: ['bootstrap', 'cdnjs', 'google', 'jsdelivr', 'jquery'],
+  cdns: ['bootstrap', 'cdnjs', 'google', 'jsdelivr'],
+  db: 'db',
+  //syncUrl: 'http://localhost:8000/data/',
+  syncUrl: 'http://jsdelivrapi-sync.aws.af.cm/data/',
+  tasks: {
+    sync: {minute: 5}
+  },
+  maxcdn: {
+    alias: 'replace this',
+    key: 'replace this',
+    secret: 'replace this',
+    zoneId: 0 // replace with some zone id
+  },
+  github: '' // replace with github token
 };
