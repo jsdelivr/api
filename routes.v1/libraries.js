3bb1c0b1d854d43310b38f8b623c88f993549d5a
austin brown <austinjbraun@gmail.com>
2015-05-11 00:39:48 -0700

    Add support for minimatch, improve error response format.

--- routes.v1/libraries.js
+++ routes.v1/libraries.js
@@ -10,25 +10,19 @@ var router = new require("express").Router()
   , dbs = require("../db")
   , api = require("../lib/api.v1");
 
-function _sendResult(err, result, res) {
+function _sendResult(err, result, req, res) {
 
   if (err) {
     var error = {
       status: err.code || 500,
-      message: err.message || err
+      message: err.message || err,
+      url: req.url
     };
     res.status(err.code || 500).json(error);
   }
   else {
     res.status(200).json(result);
   }
-}
-
-function _buildActionParams(query) {
-
-  var queryFields = ["name", "zip", "mainfile", "lastversion", "versions", "description", "homepage", "github", "author", "assets"]
-    , params = _.pick(query, queryFields);
-  return params;
 }
 
 router.param("cdn", function (req, res, next) {
@@ -41,14 +35,16 @@ router.param("cdn", function (req, res, next) {
     next();
   }
   else {
-    res.status(400).json({status: 400, error: "Invalid library requested: " + cdn});
+    var error = new Error("Invalid CDN requested: " + cdn);
+    error.code = 400;
+    _sendResult(error, {}, req, res);
   }
 });
 
 router.get("/:cdn/libraries", function (req, res) {
 
-  api.processRequest(req.collection, req.query, api.actions.find, _buildActionParams(req.query), function (err, result) {
-    _sendResult(err, result, res);
+  api.processRequest(req.collection, req.query, api.actions.find, function (err, result) {
+    _sendResult(err, result, req, res);
   });
 });
 
@@ -56,8 +52,8 @@ router.get("/:cdn/libraries/:name", function (req, res) {
 
   req.query.name = req.params.name;
 
-  api.processRequest(req.collection, req.query, api.actions.findOne, _buildActionParams(req.query), function (err, result) {
-    _sendResult(err, result, res);
+  api.processRequest(req.collection, req.query, api.actions.findOne, function (err, result) {
+    _sendResult(err, result, req, res);
   });
 });
 
@@ -66,8 +62,8 @@ router.get("/:cdn/libraries/:name/:version", function (req, res) {
   req.query.name = req.params.name;
   req.query.version = req.params.version;
 
-  api.processRequest(req.collection, req.query, api.actions.findOne, _buildActionParams(req.query), function (err, result) {
-    _sendResult(err, result, res);
+  api.processRequest(req.collection, req.query, api.actions.findOne, function (err, result) {
+    _sendResult(err, result, req, res);
   });
 });
 
