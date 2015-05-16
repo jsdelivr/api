9279c17c6ec642a2d6eed40fe649625dbbd54fc9
austin brown <austinjbraun@gmail.com>
2015-05-16 11:22:38 -0700

    Sync jsdelivr before cdnjs.

--- config/config.template.js
+++ config/config.template.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: 8001,
   //cdns: ['bootstrap', 'cdnjs', 'google', 'jsdelivr', 'jquery'],
-  cdns: ['bootstrap', 'cdnjs', 'google', 'jsdelivr'],
+  cdns: ['bootstrap', 'jsdelivr', 'cdnjs', 'google'],
   //cdns: ['bootstrap','google'],
   db: 'db',
   //syncUrl: 'http://localhost:8000/data/',
