884fe34acce720076fcb1a92d7d24cffdd236a24
austin brown <austinjbraun@gmail.com>
2015-06-16 22:40:41 -0700

    Add support for conditional requests - closes #72

--- routes.v2/libraries.js
+++ routes.v2/libraries.js
@@ -22,7 +22,34 @@ function _sendResult(err, result, req, res) {
     res.status(error.status).json(error);
   }
   else {
+    // attach any header values contained in the result
+    if (result._headers) {
+      res.set(result._headers);
+      delete  result._headers;
+    }
+
     res.status(200).json(result);
+  }
+}
+
+function _eTagCheck(req, res, next) {
+
+  var etagHeader = 'if-none-match';
+  if (req.headers[etagHeader]) {
+
+    var etagsCollection = dbs[config.etagsCollection]
+      , cdnCache = etagsCollection.findOne({'cdn': req.collection.name}) || {etags: []}
+      , etagObj = _.find(cdnCache.etags, {etag: req.headers[etagHeader]});
+
+    if (etagObj) {
+      res.status(304).end();
+    }
+    else {
+      next();
+    }
+  }
+  else {
+    next();
   }
 }
 
@@ -51,14 +78,14 @@ router.get('/:cdn/libraries', function (req, res) {
   });
 });
 
-router.get('/:cdn/library', function (req, res) {
+router.get('/:cdn/library', [_eTagCheck], function (req, res) {
 
   api_v2.processRequest(req.collection, req.query, api_v2.actions.findOne, false, function (err, result) {
     _sendResult(err, result, req, res);
   });
 });
 
-router.get('/:cdn/library/:name', function (req, res) {
+router.get('/:cdn/library/:name', [_eTagCheck], function (req, res) {
 
   req.query.name = req.params.name;
 
@@ -67,7 +94,7 @@ router.get('/:cdn/library/:name', function (req, res) {
   });
 });
 
-router.get('/:cdn/library/:name/:version', function (req, res) {
+router.get('/:cdn/library/:name/:version', [_eTagCheck], function (req, res) {
 
   req.query.name = req.params.name;
   req.query.version = req.params.version;
